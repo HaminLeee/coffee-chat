d18f5aacb6381fad670edf15012b437e940af7ca
haminthecoder <jham.lee@mail.utoronto.ca>
2020-10-28 22:17:51 -0400

    Add progress to #1 #2 #3, sidebar implementation

--- client/src/components/Sidebar.js
+++ client/src/components/Sidebar.js
@@ -1,16 +1,19 @@
 import React, { Component } from 'react';
 
 class Sidebar extends Component {
+     state = {
+          menuList: ['Your Profile', 'Explore', 'Organization 1', '...']
+     }
      render() {
+          const menus = this.state.menuList.map(menu => {
+               return (
+                    <li className="sideBarMenu">{menu}</li>
+               )
+          })
           return (
                <div className="sideBar">
-                    <ul>
-                         <li>
-                              Explore
-                         </li>
-                         <li>
-                              Organizations
-                         </li>
+                    <ul className="sideBarMenuList">
+                         {menus}
                     </ul>
                     
                </div>
