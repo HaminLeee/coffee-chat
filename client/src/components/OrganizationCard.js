2d331e24f35946cd0e8a3955c301260af95c9f43
targuli1 <umid.targuliyev@mail.utoronto.ca>
2020-10-29 21:54:07 +0400

    Modified organization cards
    added organization.css
    modified OrganizationCard.js

--- client/src/components/OrganizationCard.js
+++ client/src/components/OrganizationCard.js
@@ -1,11 +1,31 @@
 import React, { Component } from 'react';
+import '../organization.css';
 
 class OrganizationCard extends Component {
+     state = {
+          organizations: ['Amazon', 'Intel', 'CSC309', 'KDMFKSD', 'SSDFPSDP'],
+
+     }
+
      render() {
+          const organizationCards = this.state.organizations.map((organization) => {
+               return (
+                   <div className="organizationCard">
+                        <div className="organizationContainer">
+                            <div>
+                                <h4><b>{organization}</b></h4>
+                            </div>
+                            <div>
+                                <button className="exploreButton"> Explore </button>
+                            </div>
+                        </div>
+                   </div>
+               )
+          })
           return (
-               <div className="card">
-                    <p>OrganizationCard</p>
-               </div>
+              <div className="organizationCards">
+                   {organizationCards}
+              </div>
           );
      }
 }
