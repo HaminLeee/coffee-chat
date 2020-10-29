faf7c9463aa210b41ede3acadaed311a4f5f84d5
haminthecoder <jham.lee@mail.utoronto.ca>
2020-10-29 19:48:53 -0400

    Added simple login page, on login goes to normal user, on admin log in goes to admin

--- client/src/pages/LoginPage.js
+++ client/src/pages/LoginPage.js
@@ -1,14 +1,26 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
 class LoginPage extends Component {
+
+
+
      render() {
           return (
                <div className="login">
                     <h1>Login</h1>
-                    <form method="post">
+                    <form>
                          <input type="text" name="u" placeholder="Username" required="required" />
                          <input type="password" name="p" placeholder="Password" required="required" />
-                         <button type="submit" class="exploreButton">Log in.</button>
+                         
+                         <Link to={ '/admin' }>
+                              <button id="loginBtn1" type="submit" className="exploreButton">Admin.</button>
+                         </Link>
+                    
+                         <Link to={ '/' }>
+                              <button type="submit" className="exploreButton">Log in.</button>
+                         </Link>
+                     
                     </form>
                </div> 
           );
