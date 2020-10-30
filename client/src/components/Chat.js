a32e4b780ec4e70219e2da5733fbb0babcbaf0d0
targuli1 <umid.targuliyev@mail.utoronto.ca>
2020-10-30 04:31:11 +0400

    Chat component
    Messaging updated

--- client/src/components/Chat.js
+++ client/src/components/Chat.js
@@ -1,22 +1,51 @@
 import React, { Component } from 'react';
 import { uid } from 'react-uid';
 import '../chat.css';
+import Message from "./Message";
 
 class Chat extends Component {
     state = {
         pageName: 'Chat',
+        active: true,
+        contacts: ["Umid", "Hamin", "Thuy", "Addie", "Trump", "Biden", "ADD"],
+        messages: [
+            {content:"Hi!", from: "Umid", to: "Thuy"},
+            {content:"Hi!", from: "Thuy", to: "Umid"},
+            {content:"Wassup!", from: "Umid", to: "Thuy"},
+            {content:"Have u started 311?", from: "Umid", to: "Thuy"}
+            ]
     }
+    constructor(props) {
+        super(props);
+        this.toggleClass = this.toggleClass.bind(this);
+    }
+    toggleClass() {
+        const newState = {pageName: this.state.packageName, active: !this.state.active};
+        this.setState( {active: !this.state.active});
+
+    };
     render() {
-        function handleClick(e) {
-            e.preventDefault();
-            // setChatAnimation(true);
-            // chatAnimation = {chatAnimation}
-        }
+        const contactList = this.state.contacts.map((contact)=> {
+           return (<li className="contact">{contact}</li>)
+        });
+
+        const messageList = this.state.messages.map((message ) => {
+          return (<Message fromMe={true} content="Hi" />)
+        });
+
         return (
-            <div className ="chat">
-                <a href="#" onClick={handleClick}>
-                    Chat
-                </a>
+            <div className={this.state.active?"chat open": "chat"} >
+               <div onClick={this.toggleClass} className="header">
+                   Chat
+               </div>
+                <div className={this.state.active?null:"hide"}>
+                    <ul className="contacts">
+                        {contactList}
+                    </ul>
+                    <ul className="messages">
+                        {messageList}
+                    </ul>
+                </div>
             </div>
         )
     }
---
+++ client/src/components/Message.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+import "../chat.css";
+
+class Message extends Component {
+
+    constructor(props) {
+        super(props);
+
+    }
+
+    render() {
+
+        if (this.props.fromMe) {
+            return (
+                <li className="message sent">
+                    <div className="messageContent">
+                        {this.props.content}
+                    </div>
+                </li>
+            );
+        }
+        else {
+            return (
+                <li className="message received">
+                    <div className="messageContent">
+                        {this.props.content}
+                    </div>
+                </li>
+            );
+        }
+    }
+}
+
+export default Message;
