699abbeebb9d4709b9bf3425baa4cb35a66692d7
haminthecoder <jham.lee@mail.utoronto.ca>
2020-11-06 12:44:30 -0500

    Add delete functionality

--- client/src/components/AdminOrganizationCard.js
+++ client/src/components/AdminOrganizationCard.js
@@ -17,6 +17,16 @@ class AdminOrganizationCard extends Component {
           })
      }
 
+     deleteOrganization = (organization) => {
+          console.log(organization);
+          let updated = this.state.organizations.filter((org) => org !== organization);
+          console.log(updated);
+
+          this.setState({
+               organizations: updated
+          })
+     }
+
      render() {
           const organizationCards = this.state.organizations.map((organization) => {
                return (
@@ -25,16 +35,23 @@ class AdminOrganizationCard extends Component {
                             <div>
                                 <h2><b>{organization}</b></h2>
                             </div>
-                            <Link to={ '/uoft' }>
-                                   <button className="exploreButton"> Explore </button>
-                            </Link>
-                        </div>
+                            <div>
+                                   <Link to={ '/uoft' }>
+                                             <button className="exploreButton"> Explore </button>
+                                   </Link>
+                                   <br></br>
+                                   <button className="delOrgButton" onClick={() => this.deleteOrganization(organization)}> Delete </button>
+                              </div>
+                         </div>
                    </div>
                )
           })
           return (
                <div>
-                    <button className="addOrgButton" onClick={this.addOrganization}> Add Organization </button>
+                    <div>
+                         <button className="addOrgButton" onClick={this.addOrganization}> Add Organization </button>
+                         <br></br>
+                    </div>
                     <div className="organizationCards">
                          {organizationCards}
                     </div>
